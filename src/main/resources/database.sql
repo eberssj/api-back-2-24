4e364579756cd2a0b8a3773e0a74a8895543ddb0
Filipe Colla <filipe10colla@gmail.com>
2024-09-12 13:40:46 -0300

    Feat: Adicionado os scripts de INSERT do MySQL

---
+++ src/main/resources/database.sql
@@ -0,0 +1,9 @@
+INSERT INTO projeto
+(referencia_projeto, empresa, objeto, descricao, coordenador, valor, data_inicio, data_termino, proposta_relatorios, contratos, artigos, situacao, id_adm)
+VALUES
+('Ref123', 'Empresa XYZ', 'Desenvolvimento de API', 'Projeto de desenvolvimento de API para gerenciamento de dados', 'João Silva', 100000.00, '2024-09-10', '2024-12-10', 'Relatório mensal', 'Contrato padrão', 'Artigo técnico', 1.0, 1);
+
+INSERT INTO adm
+(nome, email, cpf, telefone, senha, tipo, data_cadastro, ativo)
+VALUES
+('João Silva', 'joao.silva@example.com', '123.456.789-00', '123456789', 'senhaSegura123', 'Administrador', '2024-09-10', 'true');
