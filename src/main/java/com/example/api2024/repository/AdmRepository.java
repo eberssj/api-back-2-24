2af86c0df7a0d037d6db1548e5096f5e4f80d5ca
Filipe Colla <filipe10colla@gmail.com>
2024-09-14 17:29:43 -0300

    Feat: Feita a função de achar o adm pelo email

--- src/main/java/com/example/api2024/repository/AdmRepository.java
+++ src/main/java/com/example/api2024/repository/AdmRepository.java
@@ -1,9 +1,14 @@
 package com.example.api2024.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.api2024.entity.Adm;
 
 public interface AdmRepository extends JpaRepository<Adm, Long> {
 
+    Optional<Adm> findByEmail(String email);
+    boolean existsByEmail(String email);
+    
 }
