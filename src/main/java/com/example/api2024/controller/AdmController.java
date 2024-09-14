3bc6c766187ea9fbd9473908df62c66094cb9126
Filipe Colla <filipe10colla@gmail.com>
2024-09-14 17:21:30 -0300

    Feat: Adicionado a funcionalidade de buscar o tipo do adm logado

--- src/main/java/com/example/api2024/controller/AdmController.java
+++ src/main/java/com/example/api2024/controller/AdmController.java
@@ -2,13 +2,16 @@ package com.example.api2024.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api2024.entity.Adm;
 import com.example.api2024.repository.AdmRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/adm")
 public class AdmController {
@@ -18,4 +21,9 @@ public class AdmController {
 
     @GetMapping("/listar")
     public List<Adm> listarAdm() { return admRepository.findAll(); }
+
+    @GetMapping("/{email}/tipo")
+    public String getTipo(@PathVariable String email) throws Exception {
+        return admRepository.findByEmail(email).orElseThrow(() -> new Exception("Adm não encontrado")).getTipo();
+    }
 }
