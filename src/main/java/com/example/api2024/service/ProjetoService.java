7432d9f48174186ed6000734eecde364d9dd1bbf
Filipe Colla <filipe10colla@gmail.com>
2024-09-28 00:38:37 -0300

    Fix: Arrumado o constructor do projeto, onde adicionei o admID e uma verificação para ver se o admID esta sendo enviado ou não

--- src/main/java/com/example/api2024/service/ProjetoService.java
+++ src/main/java/com/example/api2024/service/ProjetoService.java
@@ -1,6 +1,7 @@
 package com.example.api2024.service;
 
 import com.example.api2024.dto.ProjetoDto;
+import com.example.api2024.entity.Adm;
 import com.example.api2024.entity.Arquivo;
 import com.example.api2024.entity.Projeto;
 import com.example.api2024.repository.ArquivoRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjetoService {
@@ -19,6 +21,9 @@ public class ProjetoService {
 
     @Autowired
     private ArquivoRepository arquivoRepository;
+
+    @Autowired
+    private AdmService admService;
 
     // Método para cadastrar um novo projeto
     public void cadastrarProjeto(ProjetoDto projetoDto, MultipartFile propostas, MultipartFile contratos, MultipartFile artigos) throws Exception {
@@ -32,7 +37,18 @@ public class ProjetoService {
         projeto.setDataInicio(projetoDto.getDataInicio());
         projeto.setDataTermino(projetoDto.getDataTermino());
         projeto.setSituacao(projetoDto.getSituacao());
-        projeto.setAdm(projetoDto.getAdm());
+
+        Optional<Adm> admOptional;
+        if (projetoDto.getAdm() != null) {
+            admOptional = admService.buscarAdm(projetoDto.getAdm()); // Busca o administrador pelo ID fornecido no DTO do projeto
+            if (admOptional.isPresent()) { // Verifica se o administrador foi encontrado
+                projeto.setAdministrador(admOptional.get()); // Define o administrador do projeto
+            } else {
+                throw new Exception("Administrador não encontrado com ID: " + projetoDto.getAdm()); // Lança uma exceção se o administrador não for encontrado
+            }
+        } else {
+            throw new Exception("ID do administrador não pode ser nulo"); // Lança uma exceção se o ID do administrador for nulo
+        }
 
         projetoRepository.save(projeto);
 
